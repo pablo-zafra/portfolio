af1390a0c49cde18e1a436a8e131972b40d425b6
pablozafra90 <pablozafra90@hotmail.com>
2025-04-15 11:25:05 +0200

    legacy-behaviour-fixed

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import Link from 'next/link';
-import logo from '../../../public/img/pablo-zafra-logo.svg';
-import Image from 'next/image';
-import BtnHamburguesa from '../BtnHamburguesa';
+import React, { useState } from "react";
+import Link from "next/link";
+import logo from "../../../public/img/pablo-zafra-logo.svg";
+import Image from "next/image";
+import BtnHamburguesa from "../BtnHamburguesa";
 
 export const NavBar: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -16,17 +16,14 @@ export const NavBar: React.FC = () => {
   return (
     <nav className="relative flex items-center justify-between py-4 px-6">
       {/* Logo (lado izquierdo) */}
-      <Link href="/" legacyBehavior>
-        <a className="flex items-center">
-          {/* <Logo className="h-8 w-auto mr-2" /> */}
-          <Image
-            className="h-8 w-auto mr-2"
-            src={logo}
-            alt="Logo"
-            width={32}
-            height={32}
-          />
-        </a>
+      <Link href="/" className="flex items-center">
+        <Image
+          className="h-8 w-auto mr-2"
+          src={logo}
+          alt="Logo"
+          width={32}
+          height={32}
+        />
       </Link>
 
       {/* Botón de menú (lado derecho) */}
@@ -34,7 +31,9 @@ export const NavBar: React.FC = () => {
 
       {/* Menú desplegable (móvil) -  Ahora está vacío, pero lo mantengo para futura expansión */}
       <div
-        className={`${menuOpen ? 'block' : 'hidden'} absolute top-full left-0 w-full bg-white shadow-md rounded-b-md md:hidden`}
+        className={`${
+          menuOpen ? "block" : "hidden"
+        } absolute top-full left-0 w-full bg-white shadow-md rounded-b-md md:hidden`}
       >
         {/* Contenido del menú irá aquí  */}
       </div>
