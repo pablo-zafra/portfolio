f5381e6af95d6a76f3281b5104ee18f1f10359dc
Pablo Zafra <hola@pablozafra.es>
2025-04-17 11:20:23 +0200

    export-core-stack-imgs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { NavBar } from "../components";
 import type { Metadata } from "next";
-import { Inter } from "@next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
