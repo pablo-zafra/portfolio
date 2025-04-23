7b50037631c414db8ca1dca8e40122144091f379
pablozafra90 <pablozafra90@hotmail.com>
2025-04-23 18:48:56 +0200

    hero-wip-08-colorCorrection

--- src/components/3dModels/Pencil.tsx
+++ src/components/3dModels/Pencil.tsx
@@ -6,20 +6,21 @@ import { useRef, useState } from "react";
 import { Vector3, Object3D } from "three";
 
 const PencilModel: React.FC = () => {
-  const { scene } = useGLTF("/3dmodels/pencil.glb");
+  const { scene } = useGLTF("/3dmodels/pencil/scene.gltf");
   const ref = useRef<Object3D>(null);
-  const [position] = useState(() => new Vector3(0, -3, 0));
+  const [position] = useState(() => new Vector3(0, -11, 0));
   const targetPosition = new Vector3(0, -1.8, 0);
   const animationSpeed = 0.05;
 
   useFrame(() => {
     if (ref.current) {
       ref.current.position.lerp(targetPosition, animationSpeed);
-      console.log(position);
     }
   });
 
-  return <primitive object={scene} dispose={null} position={position} />;
+  return (
+    <primitive ref={ref} object={scene} dispose={null} position={position} />
+  );
 };
 
 export const Pencil: React.FC = () => {
@@ -27,15 +28,15 @@ export const Pencil: React.FC = () => {
     <div style={{ width: "100%", height: "100%" }}>
       <Canvas dpr={[1, 2]} camera={{ position: [0, 15, 20], fov: 11 }}>
         <ambientLight intensity={0.6} />
-        <directionalLight position={[10, 10, 5]} intensity={1.1} />
-        <Environment preset="sunset" background={false} />
+        <directionalLight position={[10, 10, 5]} intensity={3} />
+        <Environment preset="forest" background={false} />
         <OrbitControls
           enableZoom={false}
           autoRotate={true}
           rotateSpeed={1}
-          autoRotateSpeed={Math.PI / 2} // Ajusta la velocidad de rotación aquí (radianes por segundo)
-          minPolarAngle={Math.PI / 2} // Fija el ángulo vertical mínimo (90 grados - mirando desde arriba)
-          maxPolarAngle={Math.PI / 2} // Fija el ángulo vertical máximo (90 grados - mirando desde arriba)
+          autoRotateSpeed={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          maxPolarAngle={Math.PI / 2}
         />
         <PencilModel />
       </Canvas>
