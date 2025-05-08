c1c54efe0227e3e2df8e91a0976c1a26e06b47ba
pablozafra90 <pablozafra90@hotmail.com>
2025-05-08 09:32:07 +0200

    responsive-05

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -7,27 +7,27 @@ export const AboutMe: React.FC = () => {
   const highlightRef01 = useHighlight();
 
   return (
-    <div>
+    <>
       <div
         className={"pb-[12%] bg-no-repeat bg-cover bg-top"}
         style={{ backgroundImage: `url(${topCurve.src})` }}
       ></div>
       <div className="flex justify-center items-center bg-gray-light">
-        <div className="flex flex-col-reverse max-w-5xl -mt-[3vw] text-gray-dark">
-          <h2 className="text-6xl text-center font-bold mt-42 mb-42">
+        <div className="flex flex-col-reverse px-10 xs:px-14 md:max-w-3xl xl:max-w-5xl mt-6 xl:-mt-[3vw] text-gray-dark">
+          <h2 className="text-3xl xs:text-4xl md:text-6xl leading-relaxed text-center font-bold px-12 my-28 xs:my-36 md:my-42">
             Based in <span ref={highlightRef01}>Málaga</span>
           </h2>
-          <div className="text-2xl">
-            <h3 className="inline-block text-sm uppercase text-s mr-6 align-middle font-medium">
+          <div className="text-lg sm:text-xl lg:text-2xl font-light">
+            <h3 className="inline-block text-[10px] md:text-sm uppercase text-s mr-[1em] align-baseline font-medium">
               About me
             </h3>
-            <p className="inline font-light align-middle">
+            <p className="inline align-middle">
               I started creating interactive animations with Flash and
               ActionScript, and later worked as a visual and web designer,
               building rich media ads, dynamic motion graphics, and engaging
               microsites and landing pages.
             </p>
-            <p className="font-light mt-11">
+            <p className="mt-11">
               I developed expertise with various CMS platforms, contributed to
               multiple web projects, specialized in front-end development, led a
               small team of designers and developers, and have kept learning
@@ -36,7 +36,7 @@ export const AboutMe: React.FC = () => {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
